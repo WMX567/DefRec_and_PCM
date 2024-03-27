a7b051a93e7f6195d4811bfa3b8cd051c3b36e78
Mengxi Wu <mengxiwu@a-10-27-26-231.dynapool.vpn.nyu.edu>
2024-03-26 21:50:23 -0700

    test

--- PointDA/data/dataloader.py
+++ PointDA/data/dataloader.py
@@ -51,9 +51,9 @@ class ScanNet(Dataset):
 
         # split train to train part and validation part
         if partition == "train":
-            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(np.int)
+            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(int)
             np.random.shuffle(self.train_ind)
-            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(np.int)
+            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(int)
             np.random.shuffle(self.val_ind)
 
         io.cprint("number of " + partition + " examples in scannet" + ": " + str(self.data.shape[0]))
@@ -108,9 +108,9 @@ class ModelNet(Dataset):
 
         # split train to train part and validation part
         if partition == "train":
-            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(np.int)
+            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(int)
             np.random.shuffle(self.train_ind)
-            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(np.int)
+            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(int)
             np.random.shuffle(self.val_ind)
 
         io.cprint("number of " + partition + " examples in modelnet : " + str(len(self.pc_list)))
@@ -157,9 +157,9 @@ class ShapeNet(Dataset):
 
         # split train to train part and validation part
         if partition == "train":
-            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(np.int)
+            self.train_ind = np.asarray([i for i in range(self.num_examples) if i % 10 < 8]).astype(int)
             np.random.shuffle(self.train_ind)
-            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(np.int)
+            self.val_ind = np.asarray([i for i in range(self.num_examples) if i % 10 >= 8]).astype(int)
             np.random.shuffle(self.val_ind)
 
         io.cprint("number of " + partition + " examples in shapenet: " + str(len(self.pc_list)))
---
+++ PointDA/scripts/s+m1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=50GB
+#SBATCH --time=48:00:00
+#SBATCH -p nvidia
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=s+m1ver6.out
+
+
+
+
+module load miniconda-nobashrc
+eval "$(conda shell.bash hook)"
+conda activate py38
+
+
+
+
+python ../trainer.py --out_path ../experiments --dataroot ../data --src_dataset shapenet --trgt_dataset modelnet --DefRec_on_src False --cls_weight 0.8 --DefRec_weight 0.2 --lr 1e-3 --wd 5e-5
